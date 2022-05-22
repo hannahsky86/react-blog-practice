1ffbcad70b3bf41db4b82a5a115d1b60cdc41e94
Hannah Roach <hannah@siimpl.io>
2022-05-21 21:02:22 -0600

    Update Blog list page. Added links to blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useEffect, useState} from "react";
 import pages from "../markdown/pages.json"
 import BlogDetails from "./blog-details";
+import {Link} from "react-router-dom";
 
 export default function Blog({route}) {
     const [expandPage, setExpandPage] = useState({key:null, state:false});
@@ -17,6 +18,7 @@ export default function Blog({route}) {
             <div id="blog_post" key={`post-${index}`} 
                  onClick={() => setExpandPage({key:pages.posts[index].key, state:!expandPage.state})}>
                 <h1 className="blog_title">{pages.posts[index].name}</h1>
+                <Link to={pages.posts[index].key}>{pages.posts[index].name}</Link>
                 <BlogDetails md_file={pages.posts[index]} expanded={expandPage}/> 
                 <br/>
             </div>
